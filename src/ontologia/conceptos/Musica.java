a26556474cda517eafc65700a5c85ee72e9c7a50
Marianp95 <m.npetinal@yahoo.com>
2017-11-02 22:14:12 +0100

    Update Musica.java

--- src/ontologia/conceptos/Musica.java
+++ src/ontologia/conceptos/Musica.java
@@ -1,20 +1,28 @@
 package ontologia.conceptos;
 
-import ontologia.Concepto; import ontologia.conceptos.habilidades.*; import ontologia.conceptos.necesidades.*; import ontologia.conceptos.*;
+import ontologia.Concepto;
+import ontologia.conceptos.habilidades.*;
+import ontologia.conceptos.necesidades.*;
+import ontologia.conceptos.*;
 import jadex.runtime.*;
 
 public class Musica extends Concepto {
 
-	private enum TiposMusica {ROCK, BACHATA, POP, ELECTRO}
+	public enum TiposMusica {
+		ROCK, POP, CLASICA
+	}
+
 	private TiposMusica tipo;
-	   
-	public Musica()
-    	{;}
-       
-    	public TiposMusica getTipo() {
-        	return tipo;
-    	}   
-    	public void setTipo(TiposMusica tipo) {
-        	this.tipo = tipo;
-    	}
+
+	public Musica() {
+		;
+	}
+
+	public TiposMusica getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TiposMusica tipo) {
+		this.tipo = tipo;
+	}
 }
