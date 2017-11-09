88158320ea284a4b46ab3eb8d9e83369c184dcd4
Marianp95 <m.npetinal@yahoo.com>
2017-11-09 09:42:46 +0100

    Update EquipoEstropeadoBailando.java

--- src/ontologia/predicados/EquipoEstropeadoBailando.java
+++ src/ontologia/predicados/EquipoEstropeadoBailando.java
@@ -1,20 +1,58 @@
 package ontologia.predicados;
 
 import ontologia.Predicado;
-import ontologia.conceptos.*;
+import ontologia.conceptos.habilidades.*;
+import ontologia.conceptos.necesidades.*;
 
 public class EquipoEstropeadoBailando extends Predicado {
 
-    private Musica musica;
+	private Energia energia;
+	private Diversion diversion;
+	private Higiene higiene;
+	private Hambre hambre;
+	private Deporte fisico;
 
-    public EquipoEstropeadoBailando() {
-    }
+	public EquipoEstropeadoBailando() {
+		;
+	}
 
-    public Musica getMusica() {
-        return musica;
-    }
+	public Diversion getDiversion() {
+		return diversion;
+	}
 
-    public void setMusica(Musica m) {
-        musica = m;
-    }
+	public void setDiversion(Diversion d) {
+		diversion = d;
+	}
+
+	public Energia getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(Energia e) {
+		energia = e;
+	}
+
+	public Hambre getHambre() {
+		return hambre;
+	}
+
+	public void setHambre(Hambre h) {
+		hambre = h;
+	}
+
+	public Higiene getHigiene() {
+		return higiene;
+	}
+
+	public void setHigiene(Higiene h) {
+		higiene = h;
+	}
+
+	public Deporte getDeporte() {
+		return fisico;
+	}
+
+	public void setDeporte(Deporte f) {
+		fisico = f;
+	}
 }
