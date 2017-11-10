31c9ffe367141d8a255777f0e1032b23f761e8ee
Marianp95 <m.npetinal@yahoo.com>
2017-11-10 18:35:22 +0100

    Update LlamadaFallida.java

--- src/ontologia/predicados/LlamadaFallida.java
+++ src/ontologia/predicados/LlamadaFallida.java
@@ -1,6 +1,8 @@
 package ontologia.predicados;
 
-import ontologia.Predicado; import ontologia.conceptos.habilidades.*; import ontologia.conceptos.necesidades.*;
+import ontologia.Predicado; 
+import ontologia.conceptos.habilidades.*; 
+import ontologia.conceptos.necesidades.*;
 
 public class LlamadaFallida extends Predicado {
     
@@ -9,33 +11,33 @@ public class LlamadaFallida extends Predicado {
 	private Carisma carisma;
 
 	public LlamadaFallida()
-    {;}
+    	{;}
 	
 	public LlamadaFallida(Energia energia, InteraccionSocial interaccion, Carisma carisma)
-    {
+    	{
 		this.energia=energia;
 		this.interaccion=interaccion;
 		this.carisma=carisma;
-    }
+    	}
 
 	public Energia getEnergia() {
-        return energia;
-    }
-    public void setEnergia(Energia e) {
-        energia=e;
-    }
+    	    return energia;
+    	}
+    	public void setEnergia(Energia e) {
+        	energia=e;
+    	}
 	
 	public InteraccionSocial getInteraccionSocial() {
-        return interaccion;
-    }
-    public void setInteraccionSocial(InteraccionSocial i) {
-        interaccion=i;
-    }
+        	return interaccion;
+    	}
+    	public void setInteraccionSocial(InteraccionSocial i) {
+        	interaccion=i;
+    	}
 	
 	public Carisma getCarisma() {
-        return carisma;
-    }
-    public void setCarisma(Carisma c) {
-        carisma=c;
-    }
+        	return carisma;
+    	}
+    	public void setCarisma(Carisma c) {
+        	carisma=c;
+    	}
 }
