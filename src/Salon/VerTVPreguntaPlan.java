6524a4d9fef6a6636585e54a0268d0aba245805b
luismdlp <luismdlp@hotmail.com>
2017-11-10 17:13:32 +0100

    - Ver TV con modificaciones para aceptar varios sims a la vez. - VerTVPreguntaPlan actualizado - VertTVRespuestaPlan actualizado

---
+++ src/Salon/VerTVPreguntaPlan.java
@@ -0,0 +1,80 @@
+package Salon;
+
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.IGoal;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import jadex.runtime.impl.RBelief;
+import jadex.runtime.impl.RBeliefbase;
+import ontologia.Accion;
+import ontologia.acciones.VerTV;
+import ontologia.conceptos.CanalTV;
+import ontologia.conceptos.necesidades.Diversion;
+import ontologia.conceptos.necesidades.Energia;
+import ontologia.predicados.TVEstropeada;
+import java.util.ArrayList;
+
+public class VerTVPreguntaPlan extends Plan {
+    /**
+     * Plan body.
+     */
+    public void body() {
+        IMessageEvent request = (IMessageEvent) getInitialEvent();
+        /*
+        Se realiza agree siempre ya que la TV nunca se encuentra ocupada
+         */
+        IMessageEvent agree = createMessageEvent("tv_no_ocupada");
+        agree.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+        sendMessage(agree);
+
+		/* Creencias */
+        RBeliefbase bb = (RBeliefbase) getBeliefbase();
+        RBelief creenciaObsolescencia = (RBelief) bb.getBelief("obsolescencia_tv");
+        Integer obsolescenciaTV = (Integer) creenciaObsolescencia.getFact();
+
+        if (obsolescenciaTV <= 0) {
+        /* Mensaje de failure que se le env�a al Sim */
+
+            VerTV content = (VerTV) request.getContent();
+            CanalTV canal = content.getCanalTV();
+            Diversion diversion = content.getDiversion();
+            Energia energia = content.getEnergia();
+
+            IMessageEvent failure = createMessageEvent("tv_estropeada");
+            TVEstropeada tvEstropeada = new TVEstropeada(canal, energia, diversion);
+            failure.setContent(tvEstropeada);
+            failure.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+            sendMessage(failure);
+
+
+        }else{
+            obsolescenciaTV = obsolescenciaTV - 1;
+            creenciaObsolescencia.setFact(obsolescenciaTV);
+
+            RBelief creenciaMensajes=(RBelief) bb.getBelief("mensajes_tv");
+            ArrayList<IMessageEvent> arrayMensajes= (ArrayList<IMessageEvent>)creenciaMensajes.getFact();
+            RBelief creenciaTiemposFinTV=(RBelief) bb.getBelief("tiempos_fin_tv");
+            ArrayList<Integer> arrayTiempos = (ArrayList<Integer>)creenciaTiemposFinTV.getFact();
+
+            RBelief creenciaTiempo=(RBelief) bb.getBelief("tiempo_actual");
+            Integer tiempo= (Integer)creenciaTiempo.getFact();
+
+            /*
+            Se actualiza el Array de Mensajes añadiendo el mensaje del sim actual en la ultima posicion
+             */
+            arrayMensajes.add(request);
+            creenciaMensajes.setFact(arrayMensajes);
+
+            /*
+            Se actualiza el array de tiempos de finalización de las TV añadiendo la ultima
+             */
+            arrayTiempos.add(tiempo+ Accion.TIEMPO_MEDIO);
+            creenciaTiemposFinTV.setFact(arrayTiempos);
+
+            if(arrayMensajes.isEmpty()){
+                IGoal goal= createGoal("leer_periodico_tiempo_superado");
+                dispatchTopLevelGoal(goal);
+            }
+        }
+    }
+}
---
+++ src/Salon/VerTVRespuestaPlan.java
@@ -0,0 +1,94 @@
+package Salon;
+
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import jadex.runtime.impl.RBelief;
+import jadex.runtime.impl.RBeliefbase;
+import ontologia.acciones.VerTV;
+import ontologia.conceptos.CanalTV;
+import ontologia.conceptos.habilidades.Habilidad;
+import ontologia.conceptos.necesidades.Diversion;
+import ontologia.conceptos.necesidades.Energia;
+import ontologia.conceptos.necesidades.Necesidad;
+import ontologia.predicados.HasVistoTV;
+
+import java.util.ArrayList;
+
+public class VerTVRespuestaPlan extends Plan {
+    /**
+     *  Plan body.
+     */
+    public void body()
+    {
+
+
+		/* Se coge el mensaje de la primera posicion del array de mensajes guardado en la creencia mensajes_tv */
+        RBeliefbase bb=(RBeliefbase) getBeliefbase();
+        RBelief creenciaMensajes=(RBelief) bb.getBelief("mensajes_tv");
+        ArrayList<IMessageEvent> arrayMensajes= (ArrayList<IMessageEvent>) creenciaMensajes.getFact();
+        IMessageEvent request = arrayMensajes.get(0);
+        /*
+        Se borra el primer mensaje del Array de Mensajes de la creencia mensajes_tv,
+        ya que es el primero en activar este plan
+         */
+        arrayMensajes.remove(0);
+        creenciaMensajes.setFact(arrayMensajes);
+
+        /*
+        Se borra la primera posición del Array de tiempos de finalización de la creencia tiempos_fin_tv
+         */
+        RBelief creenciaTiempoTV=(RBelief) bb.getBelief("tiempos_fin_tv");
+        ArrayList<Integer> arrayTiempos = (ArrayList<Integer>) creenciaTiempoTV.getFact();
+        arrayTiempos.remove(0);
+        creenciaTiempoTV.setValue(arrayTiempos);
+
+        /*
+        Si la tv no esta ocupado se termina el goal ya que no hay que comprabar la condición indicada en maintaincondition
+        ya que no hay ningún sim utilizandolo
+        Si estuviera ocuapada no habría que terminar el goal ya que habría que se actualizaría los valores de la condicion
+        con los datos del siguiente Sim. Volviendose a meter en este plan para comprobar devolverle un inform cuando termine de
+        ver la TV
+         */
+        if(arrayTiempos.isEmpty()) {
+            getGoalbase().getGoal("ver_tv_tiempo_superado").drop();
+        }
+
+        VerTV content = (VerTV)request.getContent();
+        CanalTV canal= content.getCanalTV();
+        Diversion diversion= content.getDiversion();
+        Energia energia= content.getEnergia();
+
+
+        /*
+         * Energia
+         */
+        energia.setGrado(content.getEnergia().getGrado()- Necesidad.NC_POCO);
+        content.setEnergia(energia);
+
+        /*
+         * Diversion
+         */
+        diversion.setGrado(content.getDiversion().getGrado()+ Necesidad.NC_NORMAL);
+        content.setDiversion(diversion);
+
+        /*
+         * Se modifican las habilidades Carisma o Cocina dependiendo del canal que solicie el Sim
+         */
+        if(canal.getCanal().equals(CanalTV.Canales.COCINA)){
+            canal.getCocina().setExperiencia(content.getCanalTV().getCocina().getExperiencia()+ Habilidad.HB_NORMAL);
+            content.getCanalTV().setCocina(canal.getCocina());
+        } else{
+            canal.getCarisma().setExperiencia(content.getCanalTV().getCarisma().getExperiencia()+Habilidad.HB_NORMAL);
+            content.getCanalTV().setCarisma(canal.getCarisma());
+        }
+
+        IMessageEvent inform = createMessageEvent("has_visto_tv");
+        HasVistoTV hasvistoTV= new HasVistoTV(canal,energia,diversion);
+        inform.setContent(hasvistoTV);
+        inform.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+        sendMessage(inform);
+    }
+}
+
+
