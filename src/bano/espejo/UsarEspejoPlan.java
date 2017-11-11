e313441eebed814af024b8f0f4cba42500173b53
Pablo Lorenzo <palelorenzo_7@hotmail.com>
2017-11-11 11:56:49 +0100

    Actualización planes viejos

--- src/bano/espejo/UsarEspejoPlan.java
+++ src/bano/espejo/UsarEspejoPlan.java
@@ -2,13 +2,14 @@ package bano.espejo;
 import ontologia.Accion;
 import ontologia.acciones.*;
 import ontologia.conceptos.habilidades.Carisma;
-import ontologia.conceptos.habilidades.Habilidad;
 import ontologia.conceptos.necesidades.Energia;
+import ontologia.conceptos.necesidades.Higiene;
 import ontologia.conceptos.necesidades.Necesidad;
 import ontologia.conceptos.necesidades.Vejiga;
-import ontologia.predicados.EspejoUsado;
+import ontologia.predicados.DuchaEstropeada;
+import ontologia.predicados.OrdenadorEstropeadoNavegarInternet;
+import ontologia.predicados.TeHasLavado;
 import ontologia.predicados.VaterEstropeado;
-import ontologia.predicados.VaterUsado;
 
 import java.util.*;
 import jadex.runtime.*;
@@ -25,22 +26,29 @@ public class UsarEspejoPlan extends Plan {
         
         Carisma carisma = content.getCarisma();
         Energia energia = content.getEnergia();
-       
-            energia.setGrado(energia.getGrado() - Necesidad.NC_POCO);
-            content.setEnergia(energia);
-
-            carisma.setExperiencia(carisma.getExperiencia() + Habilidad.HB_NORMAL);
-            content.setCarisma(carisma);
-            
-        try {
-            wait(Accion.TIEMPO_MEDIO);
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
+        
+        Boolean ocupado = (Boolean)getBeliefbase().getBelief("ocupado").getFact();
+        
+        if(ocupado.booleanValue()) {
+            IMessageEvent respuesta = createMessageEvent("espejo_ocupado");
+            respuesta.setContent(content);
+            sendMessage(respuesta);
         }
-        IMessageEvent respuesta = createMessageEvent("espejo_usado");
-        EspejoUsado espejoUsado = new EspejoUsado(energia, carisma);
-        respuesta.setContent(espejoUsado);
-        sendMessage(respuesta);
-    }
+        
+        else {
+            getBeliefbase().getBelief("ocupado").setFact(Boolean.TRUE);
+            int experiencia_carisma = content.getCarisma().getExperiencia();
+            int nivel_carisma = content.getCarisma().getNivel();      
+            int grado_energia = content.getEnergia().getGrado();
+            int end_timer = (int) System.currentTimeMillis() + Accion.TIEMPO_MEDIO;
+
+           
+            getBeliefbase().getBelief("experiencia_carisma").setFact(new Integer(experiencia_carisma));
+            getBeliefbase().getBelief("nivel_carisma").setFact(new Integer(nivel_carisma));
+            getBeliefbase().getBelief("energia").setFact(new Integer(grado_energia));
+            getBeliefbase().getBelief("tiempoFinalizacion").setFact(new Integer(end_timer));
+
+        }
+       
 }
-  
+}
--- src/bano/jacuzzi/CompartirJacuzziPlan.java
+++ src/bano/jacuzzi/CompartirJacuzziPlan.java
@@ -2,12 +2,10 @@ package bano.jacuzzi;
 import ontologia.Accion;
 import ontologia.acciones.*;
 import ontologia.conceptos.necesidades.Energia;
+import ontologia.conceptos.necesidades.Higiene;
 import ontologia.conceptos.necesidades.InteraccionSocial;
 import ontologia.conceptos.necesidades.Necesidad;
-import ontologia.conceptos.necesidades.Vejiga;
-import ontologia.predicados.HasCompartidoJacuzzi;
 import ontologia.predicados.JacuzziEstropeadoCompartir;
-import ontologia.predicados.JacuzziEstropeadoLavarse;
 import ontologia.predicados.TeHasLavado;
 
 import java.util.*;
@@ -20,39 +18,50 @@ import jadex.adapter.fipa.*;
 public class CompartirJacuzziPlan extends Plan {
 
     public void body(){
-    	
-        RMessageEvent peticion = ((RMessageEvent)getInitialEvent());
+    	RMessageEvent peticion = ((RMessageEvent)getInitialEvent());
         CompartirJacuzzi content = (CompartirJacuzzi)peticion.getContent();
         
         InteraccionSocial interaccionsocial = content.getInteraccionSocial();
         Energia energia = content.getEnergia();
-       
-        // Disminuye en uno la cantidad de usos restantes hasta el deterioro del jacuzzi.
-        Integer obsolescencia = (Integer) getBeliefbase().getBelief("obsolescencia").getFact() - 1;
-        if (obsolescencia <= 0) {
-            IMessageEvent respuesta = createMessageEvent("jacuzzi_estropeado_compartir");
-            JacuzziEstropeadoCompartir jacuzziEstropeadoCompartir = new JacuzziEstropeadoCompartir();
-            respuesta.setContent(jacuzziEstropeadoCompartir);
+        
+        Boolean ocupado = (Boolean)getBeliefbase().getBelief("ocupado").getFact();
+        Boolean estropeado = (Boolean)getBeliefbase().getBelief("estropeado").getFact();
+        
+        if(ocupado.booleanValue()) {
+            IMessageEvent respuesta = createMessageEvent("jacuzzi_ocupado");
+            respuesta.setContent(content);
+            sendMessage(respuesta);
+        }
+        else if (estropeado.booleanValue()) {
+            IMessageEvent respuesta = createMessageEvent("ducha_estropeada");
+            JacuzziEstropeadoCompartir response = new JacuzziEstropeadoCompartir();
+            
+            response.setInteraccionSocial(interaccionsocial);
+            response.setEnergia(energia);
+            sendMessage(respuesta);
+        }
+        else if (((Integer) getBeliefbase().getBelief("obsolescencia").getFact()).intValue() - 1<=0){
+            getBeliefbase().getBelief("estropeado").setFact(Boolean.TRUE);
+            IMessageEvent respuesta = createMessageEvent("jacuzzi_estropeado");
+            JacuzziEstropeadoCompartir response = new JacuzziEstropeadoCompartir();
+            response.setInteraccionSocial(interaccionsocial);
+            response.setEnergia(energia);
             sendMessage(respuesta);
         }
         else {
-            getBeliefbase().getBelief("obsolescencia").setFact(obsolescencia);
+            getBeliefbase().getBelief("ocupado").setFact(Boolean.TRUE);
+            int obsolescencia = ((Integer) getBeliefbase().getBelief("obsolescencia").getFact()).intValue() - 1;
+            getBeliefbase().getBelief("obsolescencia").setFact(new Integer (obsolescencia));
+            int grado_interaccionsocial = content.getInteraccionSocial().getGrado();
+            int grado_energia = content.getEnergia().getGrado();
+            int end_timer = (int) System.currentTimeMillis() + Accion.TIEMPO_MEDIO;
 
-            energia.setGrado(energia.getGrado() - Necesidad.NC_POCO);
-            content.setEnergia(energia);
+           
+            getBeliefbase().getBelief("higiene").setFact(new Integer(grado_interaccionsocial));
+            getBeliefbase().getBelief("energia").setFact(new Integer(grado_energia));
+            getBeliefbase().getBelief("tiempoFinalizacion").setFact(new Integer(end_timer));
 
-            interaccionsocial.setGrado(interaccionsocial.getGrado() + Necesidad.NC_NORMAL);
-            content.setInteraccionSocial(interaccionsocial);
-            
-        try {
-            wait(Accion.TIEMPO_MEDIO);
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
         }
-        IMessageEvent respuesta = createMessageEvent("has_compartido_jacuzzi");
-        HasCompartidoJacuzzi hasCompartidoJacuzzi = new HasCompartidoJacuzzi(energia, interaccionsocial);
-        respuesta.setContent(hasCompartidoJacuzzi);
-        sendMessage(respuesta);
-    }
+       
 }
 }
