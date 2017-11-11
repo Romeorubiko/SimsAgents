8c6f3a835f490a6b2deb609fa3ab5435e7791d55
Marianp95 <m.npetinal@yahoo.com>
2017-11-11 13:59:25 +0100

    Add files via upload

---
+++ src/Salon/EquipoMusica/BailarPreguntaPlan.java
@@ -0,0 +1,125 @@
+package Salon.EquipoMusica;
+
+import java.util.ArrayList;
+import EquipoMusica.Bailar;
+import EquipoMusica.Musica;
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.*;
+import jadex.runtime.impl.RBelief;
+import ontologia.Accion;
+import ontologia.conceptos.habilidades.Deporte;
+import ontologia.conceptos.necesidades.Diversion;
+import ontologia.conceptos.necesidades.Energia;
+import ontologia.conceptos.necesidades.Hambre;
+import ontologia.conceptos.necesidades.Higiene;
+import ontologia.predicados.EquipoEstropeadoBailando;
+
+public class BailarPreguntaPlan extends Plan {
+
+	public void body() {
+		/* Se obtiene el contenido del request */
+		IMessageEvent request = (IMessageEvent) getInitialEvent();
+		Bailar content = (Bailar) request.getContent();
+		Musica musicaPedida = content.getMusica();
+		Energia energia = content.getEnergia();
+		Higiene higiene = content.getHigiene();
+		Hambre hambre = content.getHambre();
+		Diversion diversion = content.getDiversion();
+		Deporte fisico = content.getDeporte();
+
+		/* Se obtienen las creencias necesarias */
+		RBelief creenciaSimsBailando = (RBelief) getBeliefbase().getBelief("sims_bailando"); // Número de sims bailando
+		RBelief creenciaMusicaSonando = (RBelief) getBeliefbase().getBelief("musica_sonando"); // El tipo de música que
+																								// está sonando
+		RBelief creenciaObsolescencia = (RBelief) getBeliefbase().getBelief("obsolescencia");// Si el equipo está
+																								// estropeado
+		int simsBailando = (int) creenciaSimsBailando.getFact();
+		Musica musicaSonando = (Musica) creenciaMusicaSonando.getFact();
+		int obsolescencia = (int) creenciaObsolescencia.getFact();
+
+		/*
+		 * Basándose en sus creencias, el equipo de música evalúa si aceptar la petición
+		 * del Sim
+		 */
+		if (simsBailando > 0 && !musicaSonando.equals(musicaPedida)) {
+			/*
+			 * Se rechaza la petición si el Sim pide bailar una música que no están bailando
+			 * el resto de Sims
+			 */
+			IMessageEvent refuse = createMessageEvent("peticion_rechazada");
+			refuse.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+			sendMessage(refuse);
+		} else {
+			IMessageEvent agree = createMessageEvent("peticion_aceptada");
+			agree.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+			sendMessage(agree);
+
+			/* Se comprueba si el equipo de música está roto */
+			if (obsolescencia <= 0) {
+				/* Si había sims bailando ya no los hay */
+				if (simsBailando > 0) {
+					creenciaSimsBailando.setFact(0);
+				}
+				/* La música deja de sonar */
+				if (!musicaSonando.equals(null)) {
+					creenciaMusicaSonando.setFact(null);
+				}
+				/* Mensaje de failure enviado con el predicado correspondiente */
+				IMessageEvent failure = createMessageEvent("equipo_estropeado_bailando");
+				EquipoEstropeadoBailando equipoEstropeado = new EquipoEstropeadoBailando(energia, diversion, higiene,
+						hambre, fisico);
+				failure.setContent(equipoEstropeado);
+				failure.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+				sendMessage(failure);
+			} else {
+				/*
+				 * Se modifican las creencias del agente sobre su obsolescencia, el número de
+				 * sims bailando y la música sonando.
+				 */
+				obsolescencia--;
+				creenciaObsolescencia.setFact(obsolescencia);
+
+				simsBailando++;
+				creenciaSimsBailando.setFact(simsBailando);
+
+				creenciaMusicaSonando.setFact(musicaPedida);
+
+				/*
+				 * Se obtienen los arrays que contienen los tiempos y mensajes de los sims que
+				 * están a la espera de que se modifiquen sus recursos
+				 */
+
+				RBelief creenciaMensajes = (RBelief) getBeliefbase().getBelief("mensajes_bailar");
+				@SuppressWarnings("unchecked")
+				ArrayList<IMessageEvent> arrayMensajes = (ArrayList<IMessageEvent>) creenciaMensajes.getFact();
+				RBelief creenciaTiemposFin = (RBelief) getBeliefbase().getBelief("tiempos_bailar");
+				@SuppressWarnings("unchecked")
+				ArrayList<Integer> arrayTiempos = (ArrayList<Integer>) creenciaTiemposFin.getFact();
+
+				/*
+				 * Se actualiza el Array de Mensajes añadiendo el mensaje del sim actual en la
+				 * última posicion
+				 */
+				arrayMensajes.add(request);
+				creenciaMensajes.setFact(arrayMensajes);
+
+				/*
+				 * Se actualiza el array de tiempos de finalización añadiendo el
+				 * tiempo de finalización de la acción para el Sim actual a la última posición
+				 */
+				RBelief creenciaTiempo = (RBelief) getBeliefbase().getBelief("tiempo_actual");
+				Integer tiempo = (Integer) creenciaTiempo.getFact();
+				arrayTiempos.add(tiempo + Accion.TIEMPO_MEDIO);
+				creenciaTiemposFin.setFact(arrayTiempos);
+
+				/* Si es el primero en bailar se lanza el objetivo */
+				if (simsBailando == 1) {
+					IGoal goal = createGoal("bailar_tiempo_superado");
+					dispatchTopLevelGoal(goal);
+				}
+
+			}
+		}
+
+	}
+}
---
+++ src/Salon/EquipoMusica/BailarRespuestaPlan.java
@@ -0,0 +1,139 @@
+package Salon.EquipoMusica;
+
+import java.util.ArrayList;
+
+import EquipoMusica.Bailar;
+import EquipoMusica.Musica;
+import EquipoMusica.Musica.TiposMusica;
+import jadex.adapter.fipa.SFipa;
+import jadex.runtime.*;
+import jadex.runtime.impl.RBelief;
+import ontologia.conceptos.habilidades.Deporte;
+import ontologia.conceptos.habilidades.Habilidad;
+import ontologia.conceptos.necesidades.Diversion;
+import ontologia.conceptos.necesidades.Energia;
+import ontologia.conceptos.necesidades.Hambre;
+import ontologia.conceptos.necesidades.Higiene;
+import ontologia.conceptos.necesidades.Necesidad;
+import ontologia.predicados.HasBailado;
+
+public class BailarRespuestaPlan extends Plan {
+
+	public void body() {
+		/*
+		 * Se coge el mensaje de la primera posicion del array de mensajes guardado en
+		 * la creencia mensajes
+		 */
+		RBelief creenciaMensajes = (RBelief) getBeliefbase().getBelief("mensajes_bailar");
+		@SuppressWarnings("unchecked")
+		ArrayList<IMessageEvent> arrayMensajes = (ArrayList<IMessageEvent>) creenciaMensajes.getFact();
+		IMessageEvent request = arrayMensajes.get(0);
+
+		/* Se obtienen los recursos del Sim del mensaje */
+		Bailar content = (Bailar) request.getContent();
+		Musica musicaPedida = content.getMusica();
+		Energia energia = content.getEnergia();
+		Higiene higiene = content.getHigiene();
+		Hambre hambre = content.getHambre();
+		Diversion diversion = content.getDiversion();
+		Deporte fisico = content.getDeporte();
+
+		/* Se modifican los recursos del Sim dependiendo del tipo de música que baile */
+		if (musicaPedida.getTipo().equals(TiposMusica.ROCK)) {
+			energia.setGrado(content.getEnergia().getGrado() - Necesidad.NC_MUCHO);
+			content.setEnergia(energia);
+
+			higiene.setGrado(content.getHigiene().getGrado() - Necesidad.NC_MUCHO);
+			content.setHigiene(higiene);
+
+			hambre.setGrado(content.getHambre().getGrado() - Necesidad.NC_MUCHO);
+			content.setHambre(hambre);
+
+			diversion.setGrado(content.getDiversion().getGrado() + Necesidad.NC_MUCHO);
+			content.setDiversion(diversion);
+
+			fisico.setNivel(content.getDeporte().getNivel() + Habilidad.HB_MUCHO);
+			content.setDeporte(fisico);
+
+		} else if (musicaPedida.getTipo().equals(TiposMusica.POP)) {
+			energia.setGrado(content.getEnergia().getGrado() - Necesidad.NC_NORMAL);
+			content.setEnergia(energia);
+
+			higiene.setGrado(content.getHigiene().getGrado() - Necesidad.NC_NORMAL);
+			content.setHigiene(higiene);
+
+			hambre.setGrado(content.getHambre().getGrado() - Necesidad.NC_NORMAL);
+			content.setHambre(hambre);
+
+			diversion.setGrado(content.getDiversion().getGrado() + Necesidad.NC_NORMAL);
+			content.setDiversion(diversion);
+
+			fisico.setNivel(content.getDeporte().getNivel() + Habilidad.HB_NORMAL);
+			content.setDeporte(fisico);
+		} else {
+
+			energia.setGrado(content.getEnergia().getGrado() - Necesidad.NC_POCO);
+			content.setEnergia(energia);
+
+			higiene.setGrado(content.getHigiene().getGrado() - Necesidad.NC_POCO);
+			content.setHigiene(higiene);
+
+			hambre.setGrado(content.getHambre().getGrado() - Necesidad.NC_POCO);
+			content.setHambre(hambre);
+
+			diversion.setGrado(content.getDiversion().getGrado() + Necesidad.NC_POCO);
+			content.setDiversion(diversion);
+
+			fisico.setNivel(content.getDeporte().getNivel() + Habilidad.HB_POCO);
+			content.setDeporte(fisico);
+		}
+
+		/*
+		 * Envío del mensaje al Sim informándole de que ha bailado con éxito a través
+		 * del predicado correspondiente
+		 */
+		IMessageEvent inform = createMessageEvent("has_bailado");
+		HasBailado hasBailado = new HasBailado(energia, higiene, diversion, hambre, fisico);
+		inform.getParameterSet(SFipa.RECEIVERS).addValue(request.getParameterSet(SFipa.SENDER).getValues());
+		inform.setContent(hasBailado);
+		sendMessage(inform);
+
+		/* Actualización de las creencias del equipo de música */
+		/*
+		 * El sim ha dejado de bailar, con lo que el número de sims bailando disminuye
+		 */
+		RBelief creenciaSimsBailando = (RBelief) getBeliefbase().getBelief("sims_bailando");
+		int simsBailando = (int) creenciaSimsBailando.getFact();
+		simsBailando--;
+		creenciaSimsBailando.setFact(simsBailando);
+
+		/*
+		 * Puesto que el Sim ha terminado de bailar se elimina la primera posición del
+		 * array de mensajes y la primera posición del array de tiempos de finalización
+		 */
+		arrayMensajes.remove(0);
+		creenciaMensajes.setFact(arrayMensajes);
+
+		RBelief creenciaTiempo = (RBelief) getBeliefbase().getBelief("tiempos_fin_bailar");
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer> arrayTiempos = (ArrayList<Integer>) creenciaTiempo.getFact();
+		arrayTiempos.remove(0);
+		creenciaTiempo.setValue(arrayTiempos);
+
+		/* Si no hay más sims bailando, entonces se apaga la música */
+		if (simsBailando == 0) {
+			RBelief creenciaMusicaSonando = (RBelief) getBeliefbase().getBelief("musica_sonando");
+			creenciaMusicaSonando.setFact(null);
+		}
+
+		/*
+		 * Si el array de tiempos está vacío, significa que no hay más sims bailando
+		 * SOLOS (puede haberlos bailando juntos) por lo tanto el objetivo
+		 * correspondiente de desactiva
+		 */
+		if (arrayTiempos.isEmpty()) {
+			getGoalbase().getGoal("bailar_tiempo_superado").drop();
+		}
+	}
+
+}
