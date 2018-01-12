784d05de25ec721532fbcf33c0262722bf2686d4
lmish001 <lmish001@ucr.edu>
2018-01-12 20:56:58 +0100

    cama

--- src/ontologia/predicados/CamaHecha.java
+++ src/ontologia/predicados/CamaHecha.java
@@ -4,23 +4,14 @@ import ontologia.Predicado;
 import ontologia.conceptos.necesidades.*;
 
 public class CamaHecha extends Predicado {
-    private Energia energia;
+
     private Higiene higiene;
 
-    public CamaHecha(Energia energia, Higiene higiene) {
-        this.energia = energia;
+    public CamaHecha(Higiene higiene) {
         this.higiene = higiene;
     }
 
     public CamaHecha() {
-    }
-
-    public Energia getEnergia() {
-        return energia;
-    }
-
-    public void setEnergia(Energia energia) {
-        this.energia = energia;
     }
 
     public Higiene getHigiene() {
