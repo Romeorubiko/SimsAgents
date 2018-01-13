e663e4fdd22e6545beb40d0c3cabad214da85a15
lmish001 <lmish001@ucr.edu>
2018-01-13 21:11:18 +0100

    Update BeberMinibarTerminarPlan.java

--- src/sala_recreativa/minibar/BeberMinibarTerminarPlan.java
+++ src/sala_recreativa/minibar/BeberMinibarTerminarPlan.java
@@ -18,27 +18,26 @@ import ontologia.predicados.HasBebido;
 
 public class BeberMinibarTerminarPlan extends Plan {
     public void body() {
+    	System.out.println("BeberMinibarPlaTerminar");
+    	int new_timer = (int) (System.currentTimeMillis()/1000 + 100000);      
 
-        //getGoalbase().getGoal("terminar_beber_minibar").drop();
-    	int new_timer = (int) (System.currentTimeMillis() + 100000);
-        getBeliefbase().getBelief("tiempo_fin_minibar").setFact(new Integer (new_timer));
-
-        RMessageEvent peticion= (RMessageEvent)getBeliefbase().getBelief("mensaje_beber_minibar").getFact();
+        IMessageEvent peticion= (IMessageEvent)getBeliefbase().getBelief("mensaje_beber_minibar").getFact();
         Beber contenido = (Beber) peticion.getContent();
 
         Diversion d = contenido.getDiversion();
         Vejiga v = contenido.getVejiga();
 
-        d.setGrado(d.getGrado()+ Necesidad.NC_POCO);
+        d.setGrado(d.getGrado()+ Necesidad.NC_NORMAL);
         v.setGrado(v.getGrado()-Necesidad.NC_POCO);
-
 
         HasBebido response = new HasBebido(contenido.getEnergia(), v, d);
 
         IMessageEvent inform = createMessageEvent("has_bebido");
-        inform.getParameterSet(SFipa.RECEIVERS).addValue(peticion.getParameterSet(SFipa.SENDER).getValues());
+        inform.getParameterSet(SFipa.RECEIVERS).addValue(peticion.getParameter(SFipa.SENDER).getValue());
         inform.setContent(response);
         sendMessage(inform);
+        
         getBeliefbase().getBelief("ocupado").setFact(Boolean.FALSE);
+        getBeliefbase().getBelief("tiempo_fin_minibar").setFact(new Integer (new_timer));
     }
 }
