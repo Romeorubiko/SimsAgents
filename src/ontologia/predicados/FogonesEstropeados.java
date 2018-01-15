f912b3a0df5105a3934b46af07e3b35383b40155
LuixVix <100317546@alumnos.uc3m.es>
2018-01-15 18:37:31 +0100

    Arreglado constructor fogones estropeados

--- src/ontologia/predicados/FogonesEstropeados.java
+++ src/ontologia/predicados/FogonesEstropeados.java
@@ -1,34 +1,47 @@
 package ontologia.predicados;
 
-import jadex.runtime.*; 
-import ontologia.Predicado; import ontologia.conceptos.habilidades.*; import ontologia.conceptos.necesidades.*; import ontologia.conceptos.*;
-
-
+import jadex.runtime.*;
+import ontologia.Predicado;
+import ontologia.conceptos.habilidades.*;
+import ontologia.conceptos.necesidades.*;
+import ontologia.conceptos.*;
 
 public class FogonesEstropeados extends Predicado {
-    
-private Energia energia;
-private Hambre hambre;
 
-	public FogonesEstropeados(){}
-	
-  public FogonesEstropeados(Energia energia, Hambre hambre)
-      {
-	  this.energia = energia;
-      this.hambre = hambre;
-	  }
+	private Energia energia;
+	private Hambre hambre;
+	private Cocina cocina;
 
-      public Energia getEnergia() {
-           return energia;
-       }
-       public void setEnergia(Energia e) {
-           energia=e;
-       }
-      
-       	public Hambre getHambre() {
-           return hambre;
-       }
-       public void setHambre(Hambre h) {
-           hambre=h;
-       }
+	public FogonesEstropeados() {
+	}
+
+	public FogonesEstropeados(Energia energia, Hambre hambre, Cocina cocina) {
+		this.cocina = cocina;
+		this.energia = energia;
+		this.hambre = hambre;
+	}
+
+	public Energia getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(Energia e) {
+		energia = e;
+	}
+
+	public Hambre getHambre() {
+		return hambre;
+	}
+
+	public void setHambre(Hambre h) {
+		hambre = h;
+	}
+
+	public Cocina getCocina() {
+		return cocina;
+	}
+
+	public void setCocina(Cocina cocina) {
+		this.cocina = cocina;
+	}
 }
